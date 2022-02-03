cc5f4baeaf79ddbe755b1ec5e8e1482ea82fbfc2
Tyler C <trchang@asu.edu>
2022-02-03 13:07:46 -0700

    Added Basic Header Design, Need to Add Functionality/Adaptability based on JSON Config still

--- src/HeaderComponent/Header.js
+++ src/HeaderComponent/Header.js
@@ -1,8 +1,24 @@
-
+import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap'
 
 function Header() {
   return (
-    <p>Header Page</p>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">CSE 545 Secure Hospital System Group 3</Navbar.Brand>
+        </Container>
+        <Nav>
+          <Container>
+            <NavDropdown title="Options" id="header-options">
+              <NavDropdown.Item href="HeaderAccount">Account</NavDropdown.Item>
+              <NavDropdown.Item href="HeaderSettings">Settings</NavDropdown.Item>
+              <NavDropdown.Item href="HeaderLogout">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Container>
+      </Nav>
+      </Navbar>
+      
+    </>
   );
 }
 
