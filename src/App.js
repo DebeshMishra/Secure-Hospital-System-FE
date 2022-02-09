1421ae64a8749d7506d8cc8cf03f2230d8fc5b1f
akhilamandava <amandav4@asu.edu>
2022-02-09 17:29:26 -0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/login"}>Hospital</Link>
+          <Link className="navbar-brand" to={"/login"}>Secure Hospital System</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
