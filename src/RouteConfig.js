cdd29f1eb52a21c9f59cf5c036304f41bf46661a
DebeshMishra <98782809+DebeshMishra@users.noreply.github.com>
2022-03-17 20:19:26 -0700

    Create User for Admin and Signup combined

--- src/RouteConfig.js
+++ src/RouteConfig.js
@@ -1,31 +1,30 @@
 import Dashboard from "./components/General/MainDashboard/Dashboard";
 import Login from "./components/General/LoginComponent/Login";
-import Signup from "./components/General/SignUpComponent/Signup";
+import CreateUser from "./components/General/CreateUserComponent/CreateUser";
 import Account from "./components/General/Account/Account";
 import EditAccount from "./components/General/Account/EditAccount";
 import Diagnosis from "./components/Patient/Diagnosis/Diagnosis";
 import Users from "./components/Admin/users/Users";
 
-
 export const routes = [
   {
     path: "/dashboard",
-    component: <Dashboard/>
+    component: <Dashboard />,
   },
   {
-    path:"/profile",
-    component: <Account/>
+    path: "/profile",
+    component: <Account />,
   },
   {
-    path : "/editAccount",
-    component: <EditAccount/>
+    path: "/editAccount",
+    component: <EditAccount />,
   },
   {
     path: "/diagnosis",
-    component: <Diagnosis />
+    component: <Diagnosis />,
   },
   {
     path: "/users",
-    component: <Users />
-  }
+    component: <Users />,
+  },
 ];
