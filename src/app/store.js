4088d0c4e1997b665d5e1686206080069c2d9248
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2022-04-05 03:11:14 -0700

    Correction

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user';
-import { combineReducers, createStore } from 'redux';
-import { sessionService } from 'redux-react-session';
 
 export default configureStore({
   reducer: {
